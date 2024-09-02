8dd9d17bf5d19ac2516523fd47ab052cade5d141
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-09-02 18:39:26 +0200

    feat: write a class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -15,3 +15,29 @@ export interface Directors extends Teacher {
 export const printTeacher = (firstName: string, lastName: string) => {
     return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+
+
+export class StudentClass {
+    constructor(public firstName: string, public lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework() {
+        return 'Currently working';
+    }
+
+    displayName() {
+        return this.firstName;
+    }
+}
+
+export interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClass;
+}
+
+export interface StudentClassInterface {
+    displayName(): string;
+    workOnHomework(): string;
+}
