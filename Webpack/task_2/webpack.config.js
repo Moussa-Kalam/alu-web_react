bb5aca5509aa03549dd145ef5a2fdd8949cb0583
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-09-06 13:55:25 +0200

    feat: add css and images to the configuration

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                use: ['file-loader', {
+                    loader: 'image-webpack-loader',
+                    options: {
+                        bypassOnDebug: true,
+                        disable: true
+                    },
+                },]
+            }
+        ],
+    },
+};
