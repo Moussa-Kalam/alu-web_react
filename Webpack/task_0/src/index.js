953c6345433844a260df45038536f6c320692aa5
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-09-06 13:00:28 +0200

    refactor: modify the code that adds three paragraphs to the body using jquery

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,5 @@
 const $ = require( "jquery" );
 
-$('body').append(`
-  <p>Holberton Dashboard</p>
-  <p>Dashboard data for the students</p>
-  <p>Copyright - Holberton School</p>
-`);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
